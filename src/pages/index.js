80369ecbf448328b566d9b1e703ba6d5f18383a4
Mukara <jmukara959@gmail.com>
2025-07-25 00:38:14 +0300

    Update index.js

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import Navigation from '../components/Navigation';
+import HeroSection from '../components/HeroSection';
+import AboutSection from '../components/AboutSection';
+import ProjectsSection from '../components/ProjectsSection';
+import ContactSection from '../components/ContactSection';
+import Footer from './components/Footer';
+
+
+const IndexPage = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+      <AboutSection />
+      <ProjectsSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default IndexPage;
