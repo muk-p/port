61ba9093b18ceba227d1dd8c7a5f61b09e29a69e
Mukara <jmukara959@gmail.com>
2025-07-25 00:38:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import Navigation from './components/Navigation';
-import HeroSection from './components/HeroSection';
-import AboutSection from './components/AboutSection';
-import ProjectsSection from './components/ProjectsSection';
-import ContactSection from './components/ContactSection';
-import Footer from './components/Footer';
+import IndexPage from "./pages";
 
 const App = () => {
   return (
-    <div className="font-sans text-primary-foreground bg-background">
-      <Navigation />
-      <HeroSection />
-      <AboutSection />
-      <ProjectsSection />
-      <ContactSection />
-      <Footer />
-    </div>
+      <IndexPage />
   );
 }
 
